21868fe46e61ace51196f395c767a14ae69e7164
nguyenthong123 <nbt1024@gmail.com>
2025-10-13 15:39:53 +0700

    fix: Use HashRouter for GitHub Pages compatibility

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // <-- Thêm dòng này
+// Bước 1: Import HashRouter thay vì BrowserRouter
+import { HashRouter } from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>  {/* <-- Bọc ở đây */}
+    {/* Bước 2: Sử dụng HashRouter để bọc App */}
+    <HashRouter>
       <App />
-    </BrowserRouter> {/* <-- Bọc ở đây */}
+    </HashRouter>
   </React.StrictMode>
 );
